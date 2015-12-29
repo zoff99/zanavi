eee358d2853c751b10f155e9e3c0161c86b604ed
Zoff <zoff99@users.noreply.github.com>
2015-12-29 20:24:49 +0100

    update 14

--- ci/do_android.sh
+++ ci/do_android.sh
@@ -61,24 +61,16 @@ DEBUG_="-fpic -ffunction-sections -fstack-protector -fomit-frame-pointer -fno-st
         make && \
         pwd && \
         cd navit
-        make apkg-release
-
-
-echo "xxxxxxxxxxxxxxxxxx"
-ls -al
-echo "xxxxxxxxxxxxxxxxxx"
-pwd
-echo "xxxxxxxxxxxxxxxxxx"
+        make apkg-release || pwd
 
         cd android-support-v7-appcompat && \
         cat local.properties |sed -e "s#/home/navit/_navit_develop/_need/SDK/_unpack/android-sdk-linux_x86#$_SDK_#" > l.txt && \
         mv l.txt local.properties && \
         cat local.properties
         cd ../android
-        
+
         pwd
         cat AndroidManifest.xml | sed -e 's#android:debuggable="true"#android:debuggable="false"#' > l.txt
         mv l.txt AndroidManifest.xml
-        cat AndroidManifest.xml | sed -e 's#android:versionName=".*"#android:versionName="2.0.40"#' > l.txt
-        mv l.txt AndroidManifest.xml
+
         ant release
