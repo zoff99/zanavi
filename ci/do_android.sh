f741cf4b1c31630d2acc5f169c945347136c3862
Zoff <zoff99@users.noreply.github.com>
2015-12-30 13:38:31 +0100

    update 28

--- ci/do_android.sh
+++ ci/do_android.sh
@@ -75,9 +75,9 @@ DEBUG_="-fpic -ffunction-sections -fstack-protector -fomit-frame-pointer -fno-st
 
         ant release
 
-# delete debug signing-key ---------------
-rm -f ~/.android/debug.keystore
-# delete debug signing-key ---------------
+######  --------------- delete debug signing-key ---------------
+###### rm -f ~/.android/debug.keystore
+######  --------------- delete debug signing-key ---------------
 
 if [ ! -f ~/.android/debug.keystore ]; then
  keytool -genkey -v -keystore ~/.android/debug.keystore -storepass android \
