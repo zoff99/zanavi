ab51b67537228e7a4048639e37912d04a2164064
Zoff <zoff99@users.noreply.github.com>
2016-01-08 10:43:14 +0100

    update 250

--- navit/android/src/com/zoffcc/applications/zanavi/ZANaviDebugReceiver.java
+++ navit/android/src/com/zoffcc/applications/zanavi/ZANaviDebugReceiver.java
@@ -690,6 +690,17 @@ public class ZANaviDebugReceiver extends BroadcastReceiver
 												}
 											}
 										}
+										
+										try
+										{
+											out.write("URL1:" + "http://www.openstreetmap.org/directions?engine=osrm_car&route=" + lat_pos + "%2C" + lon_pos + "%3B"+ lat_dst + "%2C"+ lon_dst + "\n");
+											out.write("URL2:" + "https://graphhopper.com/maps/?point=" + lat_pos + "%2C" + lon_pos + "&point=" + lat_dst + "%2C"+ lon_dst + "\n");
+											out.write("URL3:" + "http://www.google.com/maps/dir/" + lat_pos + "," + lon_pos + "/" + lat_dst + "," + lon_dst "\n");
+										}
+										catch (Exception e)
+										{
+											System.out.println("_DREX_:046" + e.getMessage());
+										}
 
 										try
 										{
