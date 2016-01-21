2e68ef6b4ff30240a9420896baadfee6f3369a30
Zoff <zoff99@users.noreply.github.com>
2016-01-21 12:15:09 +0100

    update verbose

--- navit/android/src/com/zoffcc/applications/zanavi/ZANaviDebugReceiver.java
+++ navit/android/src/com/zoffcc/applications/zanavi/ZANaviDebugReceiver.java
@@ -1008,7 +1008,9 @@ public class ZANaviDebugReceiver extends BroadcastReceiver
 			tttt.start();
 			Thread.sleep(25000);
 			// save screenshot
+			System.out.println("SCREENSHOT 001");
 			Navit.take_phone_screenshot("/sdcard/a.png");
+			System.out.println("SCREENSHOT 002");
 
 			if (1 == 2-1)
 			{
