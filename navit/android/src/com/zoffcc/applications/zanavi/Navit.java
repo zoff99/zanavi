6b0bf0edec67b31ed3c6e789045f1c01b6c75d13
Zoff <zoff99@users.noreply.github.com>
2016-01-22 14:54:04 +0100

    add var

--- navit/android/src/com/zoffcc/applications/zanavi/Navit.java
+++ navit/android/src/com/zoffcc/applications/zanavi/Navit.java
@@ -261,6 +261,7 @@ public class Navit extends ActionBarActivity implements Handler.Callback, Sensor
 	static ZANaviPrefs p_old = new ZANaviPrefs();
 	static final int STREET_SEARCH_STRINGS_SAVE_COUNT = 10;
 	static boolean search_ready = false;
+	static boolean search_list_ready = false;
 
 	// static AnimationDrawable mFrameAnimation;
 	static Menu cur_menu = null;
