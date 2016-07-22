36f16f6c103d378e9c4529371802fcdc68754b9f
Zoff <zoff99@users.noreply.github.com>
2016-07-22 13:39:46 +0200

    more info

--- navit/navigation.c
+++ navit/navigation.c
@@ -3273,8 +3273,14 @@ static int maneuver_required2(struct navigation *nav, struct navigation_itm *old
 	d = angle_delta(old->angle_end, new->way.angle2);
 	original_d = d;
 
+
+	tests_dbg(0, "lat,lon: %f,%f", 16.35f, 48.12f);
+
 	tests_dbg(0, "delta=%d delta_real=%d d=%d", *delta, *delta_real, d);
 	tests_dbg(0, "old->angle_end=%d new->way.angle2=%d", old->angle_end, new->way.angle2);
+	tests_dbg(0, "from type=%s", item_to_name(old->way.item.type));
+	tests_dbg(0, "to type=%s", item_to_name(new->way.item.type));
+
 
 	//long long wayid_old = navigation_item_get_wayid(&(old->way));
 	//long long wayid_new = navigation_item_get_wayid(&(new->way));
