fbc6259d7b1816a8674ca8cffcdf6d7be207f65d
Zoff <zoff99@users.noreply.github.com>
2016-07-24 21:49:05 +0200

    add code

--- navit/navit2.c
+++ navit/navit2.c
@@ -17,10 +17,80 @@
  * Boston, MA  02110-1301, USA.
  */
  
- #include "config.h"
+
+#define _USE_MATH_DEFINES 1
+#include "config.h"
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <fcntl.h>
+#include <glib.h>
+#include <math.h>
+#include <time.h>
+#include "debug.h"
+#include "navit.h"
+#include "callback.h"
+#include "gui.h"
+#include "item.h"
+#include "projection.h"
+#include "map.h"
+#include "mapset.h"
+#include "main.h"
+#include "coord.h"
+#include "point.h"
+#include "transform.h"
+#include "param.h"
+#include "menu.h"
+#include "graphics.h"
+#include "popup.h"
+#include "data_window.h"
+#include "route.h"
+#include "navigation.h"
+#include "speech.h"
+#include "track.h"
+#include "vehicle.h"
+#include "layout.h"
+#include "log.h"
+#include "attr.h"
+#include "event.h"
+#include "file.h"
+#include "profile.h"
+#include "command.h"
+#include "navit_nls.h"
+#include "map.h"
+#include "util.h"
+#include "messages.h"
+#include "vehicleprofile.h"
+#include "sunriset.h"
+#include "bookmarks.h"
+#include "map.h"
+#ifdef HAVE_API_WIN32_BASE
+#include <windows.h>
+#include "util.h"
+#endif
+#ifdef HAVE_API_WIN32_CE
+#include "libc.h"
+#endif
+
  
- int main(int argc, char **argv)
- {
-  return 0;
- }
+int main(int argc, char **argv)
+{
+	struct coord_geo g;
+	struct coord c;
+	transform_from_geo(projection_mg, &g, &c);
+
+	struct pcoord pc;
+	pc.x = c.x;
+	pc.y = c.y;
+	pc.pro = projection_mg;
+
+	navit_set_position(global_navit, &pc);
+
+	return 0;
+}
  
+
