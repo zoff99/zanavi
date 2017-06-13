1bbacc58c3b1a361e3a870d4c68c2025e565300e
Zoff <zoff99@users.noreply.github.com>
2017-06-13 19:57:45 +0200

    ignore errors

--- ci/fdroid_prebuild.sh
+++ ci/fdroid_prebuild.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
-echo '<project name="ZANavi"><target name="clean"/></project>' > build.xml && \
-mkdir -p po/lp/navit-orig-import && \
-rm navit/maptool/poly2tri-c/001/seidel-1.0/triangulate && \
-rm pngout-static && \
-rm -Rf navit/material-intro && \
-rm -Rf navit/appintro && \
+echo '<project name="ZANavi"><target name="clean"/></project>' > build.xml
+mkdir -p po/lp/navit-orig-import
+rm navit/maptool/poly2tri-c/001/seidel-1.0/triangulate
+rm pngout-static
+rm -Rf navit/material-intro
+rm -Rf navit/appintro
 rm -Rf navit/support/espeak/espeak-data/*
- 
